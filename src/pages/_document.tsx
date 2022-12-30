7eb455d150d6ba27677cf6cfe959b0f00a718ac2
anacvignola <anacvignola@riseup.net>
2022-12-30 02:40:22 -0300

    feat: add template

---
+++ src/pages/_document.tsx
@@ -0,0 +1,41 @@
+ import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+ import { extractCritical } from "@emotion/server";
+
+ export default class MyDocument extends Document {
+   static async getInitialProps(ctx: DocumentContext): Promise<any> {
+     const initialProps = await Document.getInitialProps(ctx);
+     const page = await ctx.renderPage();
+     const styles = extractCritical(page.html);
+     return { ...initialProps, ...page, ...styles };
+   }
+
+   render() {
+     return (
+       <Html lang="en">
+         <Head>
+         <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="AnVel"
+          />
+          <meta
+            name="title"
+            content="AnVel"
+          />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+           <style
+             data-emotion-css={this.props.ids.join(" ")}
+             dangerouslySetInnerHTML={{ __html: this.props.css }}
+           />
+         </Head>
+         <body>
+           <Main />
+           <NextScript />
+         </body>
+       </Html>
+     );
+   }
+ }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx}", "./src/components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        trueGray: colors.neutral,
+        primary800: '#1E3CE8',
+        primary600: '#4562F8',
+        darker: '#3B4252',
+        back: 'hsl(200, 7.0%, 8.8%)'
+      },
+    },
+    fontFamily: {
+      sans: ["IBM Plex Mono", 'monospace'],
+    },
+  },
+  plugins: [require("@tailwindcss/aspect-ratio")],
+};
