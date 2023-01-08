aa1f72bb00689efeca27e8762d624ae52349a844
anacvignola <anacvignola@riseup.net>
2023-01-08 06:58:49 +0000

    Deploying to public from @ AV-42/av-42.github.io@35d3a2ba877b2395897ad5e35b9df5c51289f3b8 🚀

---
+++ _next/static/PINcNzi6qzlc4anM_qA7C/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/185-f35f4f989d642b07.js","static/chunks/pages/index-414eaa9344ba9f0e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
