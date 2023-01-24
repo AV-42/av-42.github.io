17d1dfb2259a424d0a5722c5fdf6a58f4a2754b4
Ana Claudia Vignola <anacvignola@riseup.net>
2023-01-24 14:55:23 -0300

    remove: removed import video component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
 import Testimonials from "../components/testimonials";
@@ -13,8 +12,6 @@ import Faq from "../components/faq";
 import PopupWidget from "../components/popupWidget";
 
 //import dynamic from "next/dynamic";
-
-// const Video = dynamic(() => import("../components/video"));
 
 // const Benefits = dynamic(() => import("../components/benefits"));
 // const Footer = dynamic(() => import("../components/footer"));
